ce908cb7a8e5b2f53041f96080571dbfaf01df41
thomyks <nagythomyks@gmail.com>
2025-03-27 11:35:52 +0100

    FE change

--- frontend/src/api/paperAPI.js
+++ frontend/src/api/paperAPI.js
@@ -1,21 +1,4 @@
-// const API_BASE = "http://34.236.147.118:8000/api/v1";
-
-// export const fetchLLMPapers = async () => {
-//   try {
-//     const response = await fetch(`${API_BASE}/papers?collection_name=LLMPapers`);
-//     if (!response.ok) throw new Error("Failed to fetch LLM papers");
-//     return await response.json();
-//   } catch (error) {
-//     console.error("API error:", error);
-//     return [];
-//   }
-// };
-
-
-// const response = await fetch(`${API_BASE}/papers/?collection_name=LLMPapers`);
-
-
-const API_BASE = "http://34.236.147.118:8000/api/v1";
+const API_BASE = "http://3.88.199.242:8000/api/v1";
 
 export const fetchLLMPapers = async () => {
   try {
@@ -27,7 +10,3 @@ export const fetchLLMPapers = async () => {
     return [];
   }
 };
-
-
-
-
